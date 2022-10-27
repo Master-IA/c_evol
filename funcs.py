20487b6aa6f4d4ac404dba178ab3816b20da3605
Diego <dimaeso@gmail.com>
2022-10-27 13:41:12 +0200

    Actualizacion_operadores

--- funcs.py
+++ funcs.py
@@ -2,7 +2,7 @@ import numpy as np
 
 INV_THRESHOLD = 0.001
 LOG_THRESHOLD = 0.001
-
+SQRT_THRESHOLD = 0.001
 class Func():
 	def __init__(self, operator, name, arity):
 		self.operator = operator
@@ -23,15 +23,25 @@ def safe_inv(x1,x2=None):
 def safe_log(x1,x2=None):
 	with np.errstate(divide='ignore', invalid='ignore'):
 		return np.where(np.abs(x1) > LOG_THRESHOLD, np.log(np.abs(x1)), 0)
+def safe_sqrt(x1,x2=None):
+	with np.errstate(divide='ignore', invalid='ignore'):
+		return np.where(np.abs(x1) > LOG_THRESHOLD, np.sqrt(np.abs(x1)), 0)
+def safe_div(x1,x2):
+	with np.errstate(divide='ignore', invalid='ignore'):
+		return np.where(np.abs(x2) > INV_THRESHOLD, x1/x2, x1)
 
-FUNC_AR1_LIST = ['inv', 'log']
-FUNC_AR2_LIST = ['add', 'sub', 'mul']
+FUNC_AR1_LIST = ['inv', 'log','sqrt']	
+FUNC_AR2_LIST = ['add', 'sub', 'mul', 'div','max','min']
 FUNC_LIST = FUNC_AR1_LIST + FUNC_AR2_LIST
 
 FUNC_DICT = {
 	'add' : Func(np.add, "add", 2),
 	'sub' : Func(np.subtract, "sub", 2),
 	'mul' : Func(np.multiply, "mul", 2),
+	'div' : Func(safe_div, "div", 2),
 	'inv' : Func(safe_inv, "inv", 1),
-	'log' : Func(safe_log, "log", 1)
+	'log' : Func(safe_log, "log", 1),
+	'max' : Func(np.maximum, "max", 2),
+	'min' : Func(np.minimum, "min", 2),
+	'sqrt' : Func(safe_sqrt, "sqrt", 1)
 }
